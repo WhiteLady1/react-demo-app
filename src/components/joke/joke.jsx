6a1eaddbeee484dc38b7e0f6c4549ac28c6a6a6f
Blanka Semanova <bsemanova@deloittece.com>
2023-10-19 15:55:48 +0200

    Add showRating as optional

--- src/components/joke/joke.jsx
+++ src/components/joke/joke.jsx
@@ -2,7 +2,7 @@
 import { HandThumbUpIcon, HandThumbDownIcon } from '@heroicons/react/24/outline';
 import './joke.scss';
 
-export const Joke = ({ setup, punchline, showRating }) => (
+export const Joke = ({ setup, punchline, showRating = false }) => (
   <div className="joke">
     <div className="joke__body">
       <p className="joke__body__setup">{setup}</p>
